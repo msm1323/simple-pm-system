14dc454a6be61aadb9e59aa5262b51312a121574
Svetlana <svetlana.magomedova.1998@gmail.com>
2023-05-30 13:23:24 +0300

    [dao] add EmployeeJdbcDaoImpl, EmployeeFilter and DbUtil; add postgresql and lombok dependency [parent-module] add postgresql dependency

---
+++ simple-pm-system-dao/src/main/java/ru/msm/pm/dao/impls/EmployeeJdbcDaoImpl.java
@@ -0,0 +1,172 @@
+package ru.msm.pm.dao.impls;
+
+import ru.msm.pm.dao.MemberDao;
+import ru.msm.pm.enums.EmployeeStatus;
+import ru.msm.pm.model.Employee;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeJdbcDaoImpl implements MemberDao<Employee, Long> {
+
+    private DataSource dataSource;
+
+    //"localhost", "postgres", "password", "pm-system", 5432
+    EmployeeJdbcDaoImpl(String serverName, String user, String pass, String dbName, int port){
+        dataSource = DbUtil.buildDataSource(serverName, user, pass, dbName, port);
+    }
+
+    @Override
+    public Employee create(Employee employee) throws Exception {
+        String insertQuery = "INSERT INTO employee (name, surname, status, patronymic, account, position, email)" +
+                " values (?, ?, ?, ?, ?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS)) {
+            fillPreparedStatement(preparedStatement, employee);
+            int i = preparedStatement.executeUpdate();
+            if (i == 0) {
+                throw new SQLException("Не удалось добавить сотрудника в таблицу employee!");
+            }
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                long id = generatedKeys.getLong(1);
+                employee.setId(id);
+            }
+            generatedKeys.close();
+        }
+
+        return employee;
+    }
+
+    @Override
+    public Employee update(Employee employee) throws Exception {
+        String updateQuery = "UPDATE employee set name=?, surname=?, status=?, patronymic=?, account=?, position=?, email=?" +
+                " WHERE id=?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(updateQuery, Statement.RETURN_GENERATED_KEYS)) {
+            fillPreparedStatement(preparedStatement, employee);
+            preparedStatement.setLong(8, employee.getId());
+            int i = preparedStatement.executeUpdate();
+            if (i == 0) {
+                throw new SQLException("Не удалось изменить сотрудника в таблице employee!");
+            }
+        }
+        return employee;
+    }
+
+    @Override
+    public Employee getById(Long id) throws Exception {
+        Employee employee;
+        String selectQuery = "SELECT * FROM employee WHERE id = " + id;
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery(selectQuery);
+            if (!rs.next()) {
+                throw new SQLException("Не удалось найти сотрудника с id=" + id + " в таблице employee!");
+            }
+            employee = buildEmployeeByRs(rs);
+        }
+        return employee;
+    }
+
+    @Override
+    public Employee deleteById(Long id) throws Exception {
+        Employee employee;
+        String deleteQuery = "DELETE FROM employee WHERE id = " + id + " RETURNING *;";
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery(deleteQuery);
+            if (!rs.next()) {
+                throw new SQLException("Не удалось удалить сотрудника с id=" + id + " в таблице employee!");
+            }
+            employee = buildEmployeeByRs(rs);
+            rs.close();
+        }
+        return employee;
+    }
+
+    @Override
+    public List<Employee> getAll() throws Exception {
+        String selectQuery = "SELECT * FROM employee";
+        List<Employee> employees = new LinkedList<>();
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery(selectQuery);
+            if (!rs.next()) {
+                throw new SQLException("Не удалось найти сотрудников в таблице employee!");
+            }
+            employees.add(buildEmployeeByRs(rs));
+            while (rs.next()) {
+                employees.add(buildEmployeeByRs(rs));
+            }
+        }
+        return employees;
+    }
+
+    public List<Employee> search(EmployeeFilter filter) {
+        String joinRole_roleCondition = "", joinPp = "";
+        String ppCondition = "", statusCondition = "";
+        Map<Integer, Object> paramMap = new HashMap<>();
+        int paramIndex = 1;
+        if (filter.getProjectId() != null) {
+            joinPp = " JOIN project_participant pp on employee.id = pp.employee";
+            ppCondition = " AND pp.project=?";
+            paramMap.put(paramIndex++, filter.getProjectId());
+        }
+        if (filter.getRole() != null) {
+            joinPp = " JOIN project_participant pp on employee.id = pp.employee";
+            joinRole_roleCondition = " JOIN role on pp.role = role.id AND role.role=?";
+            paramMap.put(paramIndex++, filter.getRole().toString());
+        }
+        if (filter.getStatus() != null) {
+            statusCondition = joinPp.equals("") ? " WHERE employee.status=?" : " AND employee.status=?";
+            paramMap.put(paramIndex++, filter.getStatus().toString());
+        }
+        String searchQuery = "SELECT employee.* FROM employee" +
+                joinPp + ppCondition +
+                joinRole_roleCondition + statusCondition;
+        List<Employee> employees;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(searchQuery)) {
+            employees = new LinkedList<>();
+            for (Map.Entry<Integer, Object> entry : paramMap.entrySet()) {
+                ps.setObject(entry.getKey(), entry.getValue());
+            }
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                employees.add(buildEmployeeByRs(rs));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return employees;   //todo выкидывать ли исключение, если запрос не вернул результатов?
+    }
+
+    private void fillPreparedStatement(PreparedStatement preparedStatement, Employee employee) throws SQLException {
+        preparedStatement.setString(1, employee.getName());
+        preparedStatement.setString(2, employee.getSurname());
+        preparedStatement.setString(3, employee.getStatus().toString());
+        preparedStatement.setString(4, employee.getPatronymic());
+        preparedStatement.setString(5, employee.getAccount());
+        preparedStatement.setString(6, employee.getPosition());
+        preparedStatement.setString(7, employee.getEmail());
+    }
+
+    private Employee buildEmployeeByRs(ResultSet rs) throws SQLException {
+        return Employee.builder()
+                .id(rs.getLong(1))
+                .name(rs.getString(2))
+                .surname(rs.getString(3))
+                .status(EmployeeStatus.valueOf(rs.getString(4)))
+                .patronymic(rs.getString(5))
+                .account(rs.getString(6))
+                .position(rs.getString(7))
+                .email(rs.getString(8)).build();
+    }
+
+}
