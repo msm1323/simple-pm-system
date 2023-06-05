600f6d760c5061fd9e62583c0049bac9937f9e89
Svetlana <svetlana.magomedova.1998@gmail.com>
2023-06-05 13:29:35 +0300

    spring-boot-starter-security dependency. [controllers] added spring-boot-starter-security dependency; SecurityConfig; [services] added spring-boot-starter-security dependency; passwordEncoder work; implements UserDetailsService; initAdmin();

--- simple-pm-system-services/src/main/java/ru/msm/pm/services/impls/EmployeeServiceImpl.java
+++ simple-pm-system-services/src/main/java/ru/msm/pm/services/impls/EmployeeServiceImpl.java
@@ -1,6 +1,11 @@
 package ru.msm.pm.services.impls;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.msm.pm.dao.EmployeeRepository;
 import ru.msm.pm.dao.EmployeeSpecification;
@@ -9,18 +14,17 @@ import ru.msm.pm.mappers.EmployeeMapper;
 import ru.msm.pm.model.Employee;
 import ru.msm.pm.services.EmployeeService;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
-public class EmployeeServiceImpl implements EmployeeService {
+public class EmployeeServiceImpl implements EmployeeService, UserDetailsService {
 
     private final EmployeeRepository employeeRepository;
-
-    @Autowired
-    EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public EmployeeDto create(CreateEmployeeDto dto) {
@@ -37,7 +41,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public EmployeeDto edit(EditEmployeeDto dto) {
+    public EmployeeDto edit(EditEmployeeDto dto) {  //todo нет проверки на null поля
+        dto.setPassword(passwordEncoder.encode(dto.getPassword())); //todo
         Employee employee = EmployeeMapper.edit(dto);
         return EmployeeMapper.map(employeeRepository.updateEmployee(employee));
     }
@@ -51,13 +56,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto getEmployeeById(GetEmployeeByIdDto dto) {
-        Employee employee = employeeRepository.getEmployeeById(dto.getId());
+        Employee employee = employeeRepository.getEmployeeById(dto.getId()).get();//todo
         return EmployeeMapper.map(employee);
     }
 
     @Override
     public EmployeeDto getEmployeeByAccount(GetEmployeeByAccountDto dto) {
-        Employee employee = employeeRepository.getEmployeeByAccount(dto.getAccount());
+        Employee employee = employeeRepository.getEmployeeByAccount(dto.getAccount()).get();//todo
         return EmployeeMapper.map(employee);
     }
 
@@ -66,8 +71,27 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Long> ids = dto.getIds();
         List<Employee> employees = new ArrayList<>(ids.size());
         for (Long id : ids) {
-            employees.add(employeeRepository.getEmployeeById(id));
+            employees.add(employeeRepository.getEmployeeById(id).get());//todo
         }
         return employees.stream().map(EmployeeMapper::map).toList();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Employee employee = employeeRepository.getEmployeeByAccount(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Employee not found"));
+        return new User(username, employee.getPassword(), Collections.emptyList());
+    }
+
+    @PostConstruct
+    public void initAdmin() {        // todo
+        if (employeeRepository.getEmployeeByAccount("admin").isEmpty()) {
+            Employee e = new Employee("Admin", "Adminoff");
+            long id = employeeRepository.createEmployee(e);
+            e.setId(id);
+            e.setAccount("admin");
+            e.setPassword(passwordEncoder.encode("root"));
+            employeeRepository.updateEmployee(e);
+        }
     }
 }
