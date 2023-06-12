2ef2a3c9c987722a87ea3d5e85517da02a63064a
Svetlana <svetlana.magomedova.1998@gmail.com>
2023-06-12 15:55:08 +0300

    [services] added: test dependencies;test-properties; EmployeeServiceImplTest

---
+++ simple-pm-system-services/src/main/test/EmployeeServiceImplTest.java
@@ -0,0 +1,85 @@
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.codec.CharSequenceEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import ru.msm.pm.dao.EmployeeRepository;
+import ru.msm.pm.dto.employee.EmployeeDto;
+import ru.msm.pm.dto.employee.FindEmployeesDto;
+import ru.msm.pm.dto.employee.GetEmployeeByAccountDto;
+import ru.msm.pm.dto.employee.GetEmployeeByIdDto;
+import ru.msm.pm.mappers.EmployeeMapper;
+import ru.msm.pm.model.Employee;
+import ru.msm.pm.services.EmployeeService;
+import ru.msm.pm.services.config.ServicesConfig;
+import ru.msm.pm.services.impls.EmployeeServiceImpl;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes = ServicesConfig.class)
+@PropertySource(value = "application-test.properties")
+@EnableAutoConfiguration
+public class EmployeeServiceImplTest{
+
+    @Spy
+    EmployeeRepository repository;
+
+    @InjectMocks
+    EmployeeServiceImpl service;
+
+    @Test
+    public void testGetEmployeeById() {
+        GetEmployeeByIdDto requestDto = new GetEmployeeByIdDto(new Random().nextLong());
+        Employee dbEmp = new Employee(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+        dbEmp.setId(requestDto.getId());
+        when(repository.getEmployeeById(requestDto.getId())).thenReturn(Optional.of(dbEmp));
+
+        EmployeeDto resultDto = service.getEmployeeById(requestDto);
+
+        assertThat(resultDto.getId()).isEqualTo(requestDto.getId());
+    }
+
+    @Test
+    public void testGetEmployeeById_negative() {
+        GetEmployeeByIdDto requestDto = new GetEmployeeByIdDto(new Random().nextLong());
+        when(repository.getEmployeeById(any())).thenThrow(new NoSuchElementException());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> service.getEmployeeById(requestDto));
+    }
+
+    @Test
+    public void testGetEmployeeByAccount() {
+        GetEmployeeByAccountDto requestDto = new GetEmployeeByAccountDto(UUID.randomUUID().toString());
+        Employee dbEmp = new Employee(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+        dbEmp.setAccount(requestDto.getAccount());
+        when(repository.getEmployeeByAccount(requestDto.getAccount())).thenReturn(Optional.of(dbEmp));
+
+        EmployeeDto resultDto = service.getEmployeeByAccount(requestDto);
+
+        assertThat(resultDto.getAccount()).isEqualTo(requestDto.getAccount());
+    }
+
+    @Test
+    public void testGetEmployeeByAccount_negative() {
+        GetEmployeeByAccountDto requestDto = new GetEmployeeByAccountDto(UUID.randomUUID().toString());
+        when(repository.getEmployeeByAccount(any())).thenThrow(new NoSuchElementException());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> service.getEmployeeByAccount(requestDto));
+
+        verify(repository, times(1)).getEmployeeByAccount(requestDto.getAccount());
+    }
+
+}
